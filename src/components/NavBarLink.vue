bdebf3aaa654dfe25d6ccadd6f710c8a66db45d6
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-12-09 19:49:41 +0100

    feat: add nav-bar-link component

---
+++ src/components/NavBarLink.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+interface Properties {
+  routeName: string;
+}
+
+defineProps<Properties>();
+</script>
+
+<template>
+  <RouterLink
+    :to="{ name: routeName }"
+    class="rounded px-2 py-1 transition-colors hover:bg-secondary"
+    exact-active-class="font-bold hover:bg-transparent"
+  >
+    <slot />
+  </RouterLink>
+</template>
+
+<style scoped></style>
