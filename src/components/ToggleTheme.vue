41055fcfce572ffaf16381e06e723f83169ab3c0
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-12-09 19:52:34 +0100

    feat: add toggle-theme component

---
+++ src/components/ToggleTheme.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import { Moon, Sun } from "lucide-vue-next";
+import { ref } from "vue";
+
+const dark = ref<boolean>(false);
+
+const toggleDark = () => (dark.value = !dark.value);
+</script>
+
+<template>
+  <Button @click="toggleDark" variant="ghost" size="icon">
+    <Sun v-if="dark" />
+    <Moon v-else />
+  </Button>
+</template>
+
+<style scoped></style>
