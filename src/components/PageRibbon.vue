feae5076cc5e3000169c398a8cbad8c80cdd62b6
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-12-10 19:37:21 +0100

    feat: add page-ribbon component

---
+++ src/components/PageRibbon.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="flex flex-wrap items-center justify-between gap-6 border-b border-b-gray-200 px-5 py-3 md:px-7 md:py-5"
+  >
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>
