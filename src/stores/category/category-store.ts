e5a4bf77c25eb3be1a1c7945e937b76169d4fbe0
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-12-11 20:27:37 +0100

    feat: add add-category action in category-store

--- src/stores/category/category-store.ts
+++ src/stores/category/category-store.ts
@@ -1,11 +1,18 @@
-import { Category } from "@/stores/category/category-types.ts";
+import { Category, CategoryData } from "@/stores/category/category-types.ts";
 import { defineStore } from "pinia";
+import { v4 as uuidv4 } from "uuid";
 import { ref } from "vue";
 
 const setupStore = () => {
   const categories = ref<Category[]>([]);
 
+  const addCategory = (categoryData: CategoryData): void => {
+    const category: Category = { ...categoryData, id: uuidv4() };
+    categories.value.push(category);
+  };
+
   return {
+    addCategory,
     categories,
   };
 };
