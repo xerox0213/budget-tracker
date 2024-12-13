ac706a914fb428954fd0abe1dd2fad38a077d65d
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-12-13 22:06:15 +0100

    refactor(transaction-schema): extract check callback outside refine

--- src/stores/transaction/transaction-schema.ts
+++ src/stores/transaction/transaction-schema.ts
@@ -1,11 +1,15 @@
 import { useCategoryStore } from "@/stores/category/category-store.ts";
 import { z } from "zod";
 
+const checkCategoryExistence = (categoryId: string) => {
+  return useCategoryStore().categoryExists(categoryId);
+};
+
 export const transactionDataSchema = z.object({
   amount: z.number().positive(),
-  categoryId: z.string().refine((categoryId) => {
-    return useCategoryStore().categoryExists(categoryId);
-  }, "The category does not exist."),
+  categoryId: z
+    .string()
+    .refine(checkCategoryExistence, "The category does not exist"),
   date: z.string().date(),
   description: z.string().optional(),
 });
