09f30745cda3a546e523008d8024f2c9b59e4d95
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-12-13 20:27:02 +0100

    feat(transaction-types): create transaction-data type from transaction-data-schema

--- src/stores/transaction/transaction-types.ts
+++ src/stores/transaction/transaction-types.ts
@@ -1,15 +1,12 @@
 import { Category } from "@/stores/category/category-types.ts";
+import { transactionDataSchema } from "@/stores/transaction/transaction-schema.ts";
+import { z } from "zod";
 
 export interface Transaction extends TransactionData {
   id: string;
 }
 
-export interface TransactionData {
-  amount: number;
-  categoryId: Category["id"];
-  date: string;
-  description?: string;
-}
+export type TransactionData = z.infer<typeof transactionDataSchema>;
 
 export interface TransactionWithCategory extends Transaction {
   category: Category;
