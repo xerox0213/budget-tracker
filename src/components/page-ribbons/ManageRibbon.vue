9b0187bc5b8d24e2e1b757704e9979b2fda78713
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-12-14 18:32:37 +0100

    feat: creation manage-ribbon component

---
+++ src/components/page-ribbons/ManageRibbon.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import PageRibbon from "@/components/PageRibbon.vue";
+import PageRibbonSubtitle from "@/components/PageRibbonSubtitle.vue";
+import PageRibbonTitle from "@/components/PageRibbonTitle.vue";
+</script>
+
+<template>
+  <PageRibbon>
+    <div>
+      <PageRibbonTitle>Manage</PageRibbonTitle>
+      <PageRibbonSubtitle>
+        Manage your account settings and categories
+      </PageRibbonSubtitle>
+    </div>
+  </PageRibbon>
+</template>
+
+<style scoped></style>
