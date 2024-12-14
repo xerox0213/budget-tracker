1cb4fbe3d1c1ebb28dbadd5bdd7e4eeb6d7f8f74
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-12-14 20:40:50 +0100

    test: add-income-category action should add a new one

---
+++ tests/stores/category-store.test.ts
@@ -0,0 +1,31 @@
+import { CategoryType } from "@/stores/category/category-schema.ts";
+import { useCategoryStore } from "@/stores/category/category-store.ts";
+import { CategoryData } from "@/stores/category/category-types.ts";
+import { createPinia, setActivePinia } from "pinia";
+import { beforeEach, describe, expect, it } from "vitest";
+
+let categoryStore: ReturnType<typeof useCategoryStore>;
+let categoryData: CategoryData;
+
+beforeEach(() => {
+  setActivePinia(createPinia());
+  categoryStore = useCategoryStore();
+  categoryData = {
+    icon: "😊",
+    name: "wow",
+  };
+});
+
+describe("add income category action", () => {
+  it("should add new income category", () => {
+    categoryStore.addIncomeCategory(categoryData);
+
+    const expected = {
+      ...categoryData,
+      categoryType: CategoryType.enum.income,
+    };
+    const result = categoryStore.categories[0];
+
+    expect(result).toMatchObject(expected);
+  });
+});
