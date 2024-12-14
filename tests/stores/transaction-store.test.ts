4225e034bbae9394305e5ec1e7aee0ce4f3b9065
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-12-14 11:54:53 +0100

    test(add-transaction): should add a new transaction

---
+++ tests/stores/transaction-store.test.ts
@@ -0,0 +1,37 @@
+import { useCategoryStore } from "@/stores/category/category-store.ts";
+import { useTransactionStore } from "@/stores/transaction/transaction-store.ts";
+import { TransactionData } from "@/stores/transaction/transaction-types.ts";
+import { createPinia, setActivePinia } from "pinia";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+
+let transactionStore: ReturnType<typeof useTransactionStore>;
+let categoryStore: ReturnType<typeof useCategoryStore>;
+
+beforeEach(() => {
+  setActivePinia(createPinia());
+  transactionStore = useTransactionStore();
+  categoryStore = useCategoryStore();
+});
+
+describe("add transaction action", () => {
+  let transactionData: TransactionData;
+
+  beforeEach(() => {
+    transactionData = {
+      amount: 22,
+      categoryId: "1",
+      date: "2024-12-14",
+      description: "ok",
+    };
+    vi.restoreAllMocks();
+  });
+
+  it("should add the transaction", () => {
+    const spy = vi.spyOn(categoryStore, "categoryExists");
+    spy.mockReturnValue(true);
+
+    transactionStore.addTransaction(transactionData);
+
+    expect(transactionStore.transactions[0]).toMatchObject(transactionData);
+  });
+});
