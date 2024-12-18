40bcf1191a9c451fc68a62c3a7787d381b442860
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-12-18 19:06:04 +0100

    feat: add expense categories card component

---
+++ src/components/ExpenseCategoriesCard.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import CategoriesCard from "@/components/CategoriesCard.vue";
+import { CategoryType } from "@/stores/category/category-schema.ts";
+import { useCategoryStore } from "@/stores/category/category-store.ts";
+
+const categoryStore = useCategoryStore();
+</script>
+
+<template>
+  <CategoriesCard
+    :categories="categoryStore.expenseCategories"
+    :type="CategoryType.enum.expense"
+  >
+  </CategoriesCard>
+</template>
+
+<style scoped></style>
