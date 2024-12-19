f8889d48c4e66fc9a2f44cedce59c02718676037
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-12-19 14:06:16 +0100

    feat: add some ui components
    Useful for building add category dialogs

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Header from "@/components/layout/Header.vue";
 import Main from "@/components/layout/Main.vue";
+import Toaster from "@/components/ui/toast/Toaster.vue";
 </script>
 
 <template>
+  <Toaster />
   <Header />
   <Main>
     <RouterView />
