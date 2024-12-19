2eea388d7d8de3e1c177cb423164e70e4518fb9d
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-12-19 14:12:54 +0100

    feat: create add expense category dialog component

---
+++ src/components/AddExpenseCategoryDialog.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import AddCategoryDialog from "@/components/AddCategoryDialog.vue";
+import { toast } from "@/components/ui/toast";
+import { CategoryType } from "@/stores/category/category-schema.ts";
+import { useCategoryStore } from "@/stores/category/category-store.ts";
+import { CategoryData } from "@/stores/category/category-types.ts";
+
+const categoryStore = useCategoryStore();
+
+const handleAddCategory = (categoryData: CategoryData) => {
+  categoryStore.addExpenseCategory(categoryData);
+  toast({
+    class: "text-emerald-500",
+    description: "Expense category created successfully 🎉.",
+  });
+};
+</script>
+
+<template>
+  <AddCategoryDialog
+    :type="CategoryType.enum.expense"
+    @add-category="handleAddCategory"
+  >
+    <template #dialog-trigger>
+      <slot />
+    </template>
+  </AddCategoryDialog>
+</template>
+
+<style scoped></style>
