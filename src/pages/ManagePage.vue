8513beda2f6d111783b0c9049c7ba166c69c67a5
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-12-19 15:03:13 +0100

    feat: add income and expense categories in manage page

--- src/pages/ManagePage.vue
+++ src/pages/ManagePage.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import ExpenseCategoriesCard from "@/components/ExpenseCategoriesCard.vue";
+import IncomeCategoriesCard from "@/components/IncomeCategoriesCard.vue";
 import ManageRibbon from "@/components/page-ribbons/ManageRibbon.vue";
+import PageSection from "@/components/PageSection.vue";
 </script>
 
 <template>
   <ManageRibbon />
+  <PageSection class="flex flex-col gap-y-4">
+    <IncomeCategoriesCard />
+    <ExpenseCategoriesCard />
+  </PageSection>
 </template>
 
 <style scoped></style>
