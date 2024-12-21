9b574306a1bbc4af56d9aef833dfb4d2d449d05d
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-12-21 14:22:05 +0100

    refactor(category-schema): extract category type enum
    Allows it to be used.

--- src/schemas/category-schema.ts
+++ src/schemas/category-schema.ts
@@ -1,7 +1,9 @@
 import { z } from "zod";
 
+export const categoryTypeEnum = z.enum(["income", "expense"]);
+
 export const categoryDataSchema = z.object({
   name: z.string().min(3),
   icon: z.string().emoji(),
-  categoryType: z.enum(["income", "expense"]),
+  categoryType: categoryTypeEnum,
 });
