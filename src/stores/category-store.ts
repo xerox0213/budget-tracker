bc1718adc59362ca4e7f9ecad627956ed9cca80f
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-12-21 13:22:58 +0100

    feat: add store category action

--- src/stores/category-store.ts
+++ src/stores/category-store.ts
@@ -1,13 +1,21 @@
 import { defineStore } from "pinia";
+import { v4 as uuidv4 } from "uuid";
 import { ref } from "vue";
 
-import type { Category } from "@/types/category-type.ts";
+import { categoryDataSchema } from "@/schemas/category-schema.ts";
+import type { Category, CategoryData } from "@/types/category-type.ts";
 
 const setup = () => {
   const categories = ref<Category[]>([]);
 
+  const storeCategory = (categoryData: CategoryData) => {
+    categoryDataSchema.parse(categoryData);
+    categories.value.push({ id: uuidv4(), ...categoryData });
+  };
+
   return {
     categories,
+    storeCategory,
   };
 };
 
