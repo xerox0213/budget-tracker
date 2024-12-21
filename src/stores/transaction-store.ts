3a328cb624ff68e3cbb41ce2bae5dc24d4ad0680
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-12-21 17:03:52 +0100

    feat(transaction-store): add destroy by category id action

--- src/stores/transaction-store.ts
+++ src/stores/transaction-store.ts
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 import { ref } from "vue";
 
 import { transactionDataSchema } from "@/schemas/transaction-schema.ts";
+import type { Category } from "@/types/category-type.ts";
 import type { Transaction, TransactionData } from "@/types/transaction-type.ts";
 
 const setup = () => {
@@ -21,10 +22,17 @@ const setup = () => {
     );
   };
 
+  const destroyByCategoryId = (categoryId: Category["id"]) => {
+    transactions.value = transactions.value.filter(
+      (transaction) => transaction.categoryId !== categoryId,
+    );
+  };
+
   return {
     transactions,
     store,
     destroy,
+    destroyByCategoryId,
   };
 };
 
