3dc555804fba82fb15cc5a680e70e58c07d1703b
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-12-21 15:21:37 +0100

    feat(transaction-store): add store action

--- src/stores/transaction-store.ts
+++ src/stores/transaction-store.ts
@@ -1,13 +1,28 @@
 import { defineStore } from "pinia";
+import { v4 as uuidv4 } from "uuid";
 import { ref } from "vue";
 
-import type { Transaction } from "@/types/transaction-type.ts";
+import { transactionDataSchema } from "@/schemas/transaction-schema.ts";
+import { useCategoryStore } from "@/stores/category-store.ts";
+import type { Transaction, TransactionData } from "@/types/transaction-type.ts";
 
 const setup = () => {
   const transactions = ref<Transaction[]>([]);
 
+  const store = (transactionData: TransactionData) => {
+    transactionDataSchema.parse(transactionData);
+    const categoryId = transactionData.categoryId;
+    const category = useCategoryStore().view(categoryId);
+    if (!category) throw new Error("The category does not exist");
+
+    const transaction: Transaction = { id: uuidv4(), ...transactionData };
+    transactions.value.push(transaction);
+    return transaction;
+  };
+
   return {
     transactions,
+    store,
   };
 };
 
