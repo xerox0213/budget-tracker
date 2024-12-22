663800635d3360199793f7f329cd889c510ca1bd
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-12-22 16:55:13 +0100

    feat: add theme button component
    It toggles the theme mode of the application.

---
+++ src/components/buttons/ThemeButton.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+import { isCurrentThemeDark, toggleTheme } from "@/services/theme-service.ts";
+
+const dark = ref<boolean>(isCurrentThemeDark());
+
+const handleClick = () => {
+  toggleTheme();
+  dark.value = !dark.value;
+};
+</script>
+
+<template>
+  <Button
+    variant="outlined"
+    size="small"
+    :icon="dark ? 'pi pi-sun' : 'pi pi-moon'"
+    @click="handleClick"
+  />
+</template>
+
+<style scoped></style>
