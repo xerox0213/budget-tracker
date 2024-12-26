7829b6a2a40d9572b478005a9f8565bac77262a7
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-12-26 21:00:28 +0100

    test(theme-service): add unit test for getSavedTheme
    Should return the theme saved in localstorage

---
+++ tests/unit/services/theme-service.test.ts
@@ -0,0 +1,20 @@
+// @vitest-environment happy-dom
+
+import { beforeEach, describe, expect, it } from "vitest";
+
+import { getSavedTheme } from "../../../src/services/theme-service";
+
+beforeEach(() => {
+  localStorage.clear();
+});
+
+describe("get saved theme", () => {
+  it("should return the theme saved in localstorage", () => {
+    const theme = "light";
+    localStorage.setItem("theme", theme);
+
+    const savedTheme = getSavedTheme();
+
+    expect(savedTheme).toEqual(theme);
+  });
+});
