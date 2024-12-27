8cc62136807f2db5849b20fe1389da4a6cfd3dbe
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-12-27 22:16:04 +0100

    feat: add page header component

---
+++ src/components/layouts/PageHeader.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="border-b py-5 border-surface">
+    <div class="container">
+      <h1 class="text-4xl font-bold">
+        <slot name="title" />
+      </h1>
+      <p class="text-muted-color">
+        <slot name="subtitle" />
+      </p>
+    </div>
+    <div>
+      <slot name="actions" />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
