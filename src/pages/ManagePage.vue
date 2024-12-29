989c3eb30a469d8c9fae681e4e10cc362e79e0f4
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-12-29 17:29:43 +0100

    feat: display income & expense categories in manage page

--- src/pages/ManagePage.vue
+++ src/pages/ManagePage.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import CategoriesDataView from "@/components/data-views/CategoriesDataView.vue";
+import MainContent from "@/components/layouts/MainContent.vue";
 import PageHeader from "@/components/layouts/PageHeader.vue";
 </script>
 
@@ -7,6 +9,11 @@ import PageHeader from "@/components/layouts/PageHeader.vue";
     <template #title>Manage</template>
     <template #subtitle>Manage you account settings and categories</template>
   </PageHeader>
+
+  <MainContent>
+    <CategoriesDataView category-type="income" />
+    <CategoriesDataView category-type="expense" />
+  </MainContent>
 </template>
 
 <style scoped></style>
