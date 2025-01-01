72771489a5785c676691bb414f77b056dbe1d672
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-01-01 18:03:07 +0100

    refactor(page-header): layout adjustments

--- src/components/layouts/PageHeader.vue
+++ src/components/layouts/PageHeader.vue
@@ -2,16 +2,18 @@
 
 <template>
   <div class="border-b py-5 border-surface">
-    <div class="container">
-      <h1 class="text-4xl font-bold">
-        <slot name="title" />
-      </h1>
-      <p class="text-muted-color">
-        <slot name="subtitle" />
-      </p>
-    </div>
-    <div>
-      <slot name="actions" />
+    <div class="container flex flex-wrap justify-between gap-2">
+      <div>
+        <h1 class="text-4xl font-bold">
+          <slot name="title" />
+        </h1>
+        <p class="text-muted-color">
+          <slot name="subtitle" />
+        </p>
+      </div>
+      <div class="flex items-start gap-x-1">
+        <slot name="actions" />
+      </div>
     </div>
   </div>
 </template>
