c147ed1b2faccaae45a8d1c5e65d59cc4baf5b56
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-01-01 12:51:06 +0100

    test(theme-service): add unit test for set light function

--- tests/unit/services/theme-service.test.ts
+++ tests/unit/services/theme-service.test.ts
@@ -2,7 +2,7 @@
 
 import { beforeEach, expect, it } from "vitest";
 
-import { setDark } from "../../../src/services/theme-service";
+import { setDark, setLight } from "../../../src/services/theme-service";
 
 beforeEach(() => {
   document.documentElement.classList.remove("dark");
@@ -13,4 +13,10 @@ it("should set theme to dark", () => {
   setDark();
   const isDark = document.documentElement.classList.contains("dark");
   expect(isDark).toBeTruthy();
+});
+
+it("should set theme to light", () => {
+  setLight();
+  const isLight = !document.documentElement.classList.contains("light");
+  expect(isLight).toBeTruthy();
 });
