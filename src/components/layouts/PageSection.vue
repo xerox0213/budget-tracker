24c00c5c212419520c7c80906494ac3e3fc236a3
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-01-03 14:16:04 +0100

    feat: add page section component

---
+++ src/components/layouts/PageSection.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="space-y-2">
+    <div class="flex flex-wrap justify-between gap-2">
+      <span class="text-2xl font-bold text-color">
+        <slot name="title" />
+      </span>
+      <div class="flex flex-row">
+        <slot name="actions" />
+      </div>
+    </div>
+
+    <div>
+      <slot name="default" />
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
