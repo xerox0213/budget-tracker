751dc2022afa364f2edf6fcf0cf1f9a3f8454e6f
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-01-03 14:16:37 +0100

    feat(category-type): add new type

--- src/types/category-type.ts
+++ src/types/category-type.ts
@@ -5,3 +5,8 @@ import type { categoryDataSchema } from "@/schemas/category-schema.ts";
 export type CategoryData = z.infer<typeof categoryDataSchema>;
 
 export type Category = { id: string } & CategoryData;
+
+export type CategoryWithAnalytics = Category & {
+  transactionAmount: number;
+  part: number;
+};
