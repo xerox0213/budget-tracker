491537ae164b8c68e0fe6ed9dbb3cb393bb83dbf
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-01-09 17:37:20 +0100

    feat(transaction-service): add new services

--- src/services/transaction-service.ts
+++ src/services/transaction-service.ts
@@ -1,3 +1,4 @@
+import { getDaysInMonth } from "@/services/date-service.ts";
 import { useCategoryStore } from "@/stores/category-store.ts";
 import type { Category } from "@/types/category-type.ts";
 import type { Transaction } from "@/types/transaction-type.ts";
@@ -20,4 +21,49 @@ export const getTotalTransactions = (transactions: Transaction[]) => {
 
 export const getBalance = (totalIncome: number, totalExpense: number) => {
   return totalIncome - totalExpense;
+};
+
+export const getAvailableYears = (transactions: Transaction[]) => {
+  const availableYears: Set<number> = new Set();
+
+  for (const transaction of transactions) {
+    availableYears.add(transaction.date.getFullYear());
+  }
+
+  return [...availableYears].sort((a, b) => a - b);
+};
+
+export const getTotalByMonth = (transactions: Transaction[], year: number) => {
+  const monthsInYear = 12;
+  const totalByMonth: number[] = new Array(monthsInYear).fill(0);
+
+  for (const transaction of transactions) {
+    const yearTransaction = transaction.date.getFullYear();
+    if (yearTransaction === year) {
+      const monthTransaction = transaction.date.getMonth();
+      totalByMonth[monthTransaction] += transaction.amount;
+    }
+  }
+
+  return totalByMonth;
+};
+
+export const getTotalByDay = (
+  transactions: Transaction[],
+  year: number,
+  month: number,
+) => {
+  const daysInMonth = getDaysInMonth(year, month);
+  const totalByDay: number[] = new Array(daysInMonth).fill(0);
+
+  for (const transaction of transactions) {
+    const yearTransaction = transaction.date.getFullYear();
+    const monthTransaction = transaction.date.getMonth();
+    if (yearTransaction === year && monthTransaction === month - 1) {
+      const dayTransaction = transaction.date.getDate() - 1;
+      totalByDay[dayTransaction] += transaction.amount;
+    }
+  }
+
+  return totalByDay;
 };
