d63d99ab5a93db474be20f6ef69c7bd20d11475e
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-01-09 17:31:32 +0100

    feat: add date type

---
+++ src/types/date-type.ts
@@ -0,0 +1,28 @@
+export type Months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export type Month =
+  | "January"
+  | "February"
+  | "March"
+  | "April"
+  | "May"
+  | "June"
+  | "July"
+  | "August"
+  | "September"
+  | "October"
+  | "November"
+  | "December";
