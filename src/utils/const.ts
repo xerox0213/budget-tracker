7a381327d518d45217e9e37e8daa318bbd66a421
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-01-09 17:36:30 +0100

    feat: add const file
    Encapsulate const variables that useful in the app

---
+++ src/utils/const.ts
@@ -0,0 +1,16 @@
+import type { Months } from "@/types/date-type.ts";
+
+export const months: Months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
